efef00bab66fc91fd84e4ff5340d3f3b260de02e
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-26 01:45:37 +0900

    Add company type

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type CompanyType = {
+  companyName: String;
+  companyLocation: String;
+  companyPhoneNumber: String;
+  companyFaxNumber: String;
+  companySector: String;
+  companyScale: String;
+  serviceType: String;
+  companyKeyword: String;
+};
