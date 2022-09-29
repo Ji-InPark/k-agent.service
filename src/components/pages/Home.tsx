90c4953f006e25b0c6628369e4e2f655fc769411
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-29 22:58:18 +0900

    Update Home.tsx

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,7 @@
 import SearchBar from '../components/SearchBar';
 import { css } from '@emotion/react';
 import Header from '../components/Header';
-import CompanyList from '../components/CompanyList';
+import CompanyList from '../components/company/CompanyList';
 import { useRecoilValue } from 'recoil';
 import { isLoadingAtoms } from '../../recoil/atoms';
 import LoadingModal from '../components/modal/LoadingModal';
