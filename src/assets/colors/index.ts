3728571b83e9d06b7d92ed10adb2daa366bf88a0
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-30 22:56:46 +0900

    Add more button and apply some css

--- src/assets/colors/index.ts
+++ src/assets/colors/index.ts
@@ -1,6 +1,8 @@
 const Colors = {
   LIGHTGRAY: '#00000066',
   DARKGRAY: '#000000B3',
+  NAVER: '#01C73C',
+  WANTED: '#C0C5EB',
 };
 
 export default Colors;
--- src/components/components/company/Company.tsx
+++ src/components/components/company/Company.tsx
@@ -1,19 +1,41 @@
 import { CompanyType } from '../../../types';
 import { css } from '@emotion/react';
+import Colors from '../../../assets/colors';
 
 type Props = {
   company: CompanyType;
 };
 
 function Company({ company }: Props) {
+  const buttonStyle = (color: String) => {
+    return css(`
+      display: flex;
+      background: ${color};
+      border: none;
+      fontSize: 1.5rem;
+      cursor: pointer;
+      text-align: center;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      width: 100%;
+      transition: 0.2s;
+      &:hover {
+        translate: 0% -15%;
+      }
+    );
+  `);
+  };
+
   return (
     <div
       css={css({
         display: 'flex',
         flexDirection: 'column',
-        maxWidth: 700,
+        maxWidth: 600,
         width: '80%',
-        marginTop: 50,
+        marginTop: 25,
+        marginBottom: 25,
         borderRadius: 10,
         boxSizing: 'border-box',
         padding: 5,
@@ -22,9 +44,13 @@ function Company({ company }: Props) {
     >
       <div
         css={css({
+          width: '100%',
           textAlign: 'center',
           fontSize: '2rem',
           fontWeight: 'bold',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
         })}
       >
         {company.companyName}
@@ -34,40 +60,96 @@ function Company({ company }: Props) {
       </div>
       <div
         css={css({
-          display: 'flex',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          fontSize: '1rem',
+          alignItems: 'center',
           alignSelf: 'center',
-          width: '70%',
-          minHeight: 100,
+          textAlign: 'center',
+          justifyContent: 'space-around',
+          width: '80%',
+          marginTop: '1rem',
+        })}
+      >
+        <div css={css({ fontSize: '1.5rem', fontWeight: 'bold' })}>{company.serviceType}</div>
+        <div>
+          <div>{company.companyScale}</div>
+          <div>{company.companySector}</div>
+        </div>
+      </div>
+      <div
+        css={css({
+          display: 'grid',
+          width: '80%',
+          alignSelf: 'center',
+          marginTop: '1rem',
+          gridTemplateRows: 'repeat(3, minmax(3rem, 3rem))',
+          gridTemplateColumns: 'repeat(2, 1fr)',
+          gap: '1rem',
         })}
       >
         <div
           css={css({
             display: 'flex',
-            flexDirection: 'column',
-            width: '70%',
-            fontSize: '1rem',
-            justifyContent: 'space-between',
+            background: Colors.NAVER,
+            border: 'none',
+            fontSize: '1.5rem',
+            cursor: 'pointer',
+            textAlign: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+            width: '100%',
+            transition: '0.2s',
+            gridRow: '1 / 2',
+            gridColumn: '1 / 3',
+            '&:hover': {
+              translate: '0% -15%',
+            },
           })}
+          onClick={() => window.open('https://map.naver.com/v5/search/' + company.companyLocation, '_blank')}
         >
-          <div css={css({ fontSize: '1.5rem', fontWeight: 'bold' })}>{company.serviceType}</div>
-          <div>
-            {company.companyScale}({company.companySector})
-          </div>
-          <div>tel: {company.companyPhoneNumber}</div>
-          <div>fax: {company.companyFaxNumber}</div>
+          <img
+            css={css({
+              maxWidth: '100%',
+              maxHeight: '80%',
+            })}
+            src="https://mir-s3-cdn-cf.behance.net/user/276/a78c6531673609.5703570bc812f.png"
+            alt="네이버 지도"
+          />
+          네이버 지도
+        </div>
+        <div css={buttonStyle(Colors.WANTED)} onClick={() => window.open('https://www.wanted.co.kr/search?query=' + company.companyKeyword, '_blank')}>
+          <img
+            css={css({
+              maxWidth: '60%',
+            })}
+            src="https://theme.zdassets.com/theme_assets/9309779/4f2fb72a20c8e2ee37a305ef38ef1d144774a8df.png"
+            alt="원티드"
+          />
         </div>
         <div
-          css={css({
-            width: '30%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          })}
+          css={buttonStyle('#FFFFF0')}
+          onClick={() => window.open('https://www.jobkorea.co.kr/Search/?stext=' + company.companyKeyword + '&tabType=corp&Page_No=1', '_blank')}
         >
-          <button onClick={() => window.open('https://map.naver.com/v5/search/' + company.companyLocation, '_blank')}>네이버 지도</button>
-          <button onClick={() => window.open('https://www.wanted.co.kr/search?query=' + company.companyKeyword, '_blank')}>원티드</button>
-          <button onClick={() => window.open('https://www.jobplanet.co.kr/search?query=' + company.companyKeyword, '_blank')}>잡플래닛</button>
+          <img
+            css={css({
+              maxWidth: '60%',
+            })}
+            src="http://company.jobkorea.co.kr/img/common/jk_logo.png"
+            alt="잡코리아"
+          />
         </div>
+        <div css={buttonStyle('#FFFFF0')} onClick={() => window.open('https://www.rocketpunch.com/companies?keywords=' + company.companyKeyword, '_blank')}>
+          <img
+            css={css({
+              maxWidth: '60%',
+            })}
+            src="https://www.venturesquare.net/wp-content/uploads/2018/04/rocketpunch-logo-horizontal.png"
+            alt="로켓펀치"
+          />
+        </div>
+        <button onClick={() => window.open('https://www.jobplanet.co.kr/search?query=' + company.companyKeyword, '_blank')}>잡플래닛</button>
       </div>
     </div>
   );
