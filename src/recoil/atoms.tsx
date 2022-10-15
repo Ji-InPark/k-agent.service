21d87ffa3b8a174e72f413bb75dc6d9b407abee8
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-10-16 00:00:02 +0900

    Add atoms

--- src/recoil/atoms.tsx
+++ src/recoil/atoms.tsx
@@ -6,6 +6,16 @@ export const companyListAtoms = atom<CompanyListType>({
   default: { companies: [], companyCount: 0 },
 });
 
+export const selectedGovernmentLocationAtoms = atom<string>({
+  key: 'selectedGovernmentLocationAtoms',
+  default: '',
+});
+
+export const selectedSectorAtoms = atom<string>({
+  key: 'selectedSectorsAtoms',
+  default: '',
+});
+
 export const isLoadingAtoms = atom<boolean>({
   key: 'isLoadingAtoms',
   default: false,
