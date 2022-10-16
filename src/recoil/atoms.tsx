57551756fbb44a42bb1d9fa2387d42c7f4da2b24
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-10-16 13:59:02 +0900

    Add atoms

--- src/recoil/atoms.tsx
+++ src/recoil/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { CompanyListType } from '../types';
 
 export const companyListAtoms = atom<CompanyListType>({
@@ -14,6 +14,20 @@ export const selectedGovernmentLocationAtoms = atom<string>({
 export const selectedSectorAtoms = atom<string>({
   key: 'selectedSectorsAtoms',
   default: '',
+});
+
+export const selectedPageNumberAtoms = atom<number>({
+  key: 'selectedPageNumberAtoms',
+  default: 0,
+});
+
+export const lastPageNumberSelector = selector<number>({
+  key: 'lastPageNumberSelector',
+  get: ({ get }) => {
+    const companyCount = get(companyListAtoms).companyCount;
+
+    return Math.ceil(companyCount / 20);
+  },
 });
 
 export const isLoadingAtoms = atom<boolean>({
