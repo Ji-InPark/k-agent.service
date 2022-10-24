3810d89c04e0d7595bf607ec059b7d7aeb48f01b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-10-24 22:07:40 +0900

    Add Array extension methods

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,8 +4,17 @@ declare global {
   interface String {
     isNullOrWhiteSpace(): boolean;
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Array<T> {
+    isEmpty(): boolean;
+  }
 }
 
 String.prototype.isNullOrWhiteSpace = function (): boolean {
   return this === null || this.match(/^ *$/) !== null;
+};
+
+Array.prototype.isEmpty = function (): boolean {
+  return this.length == 0;
 };
