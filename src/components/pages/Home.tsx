ceb0f0b798636d409f364ea78ee09faea1983a41
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-10-28 22:45:57 +0900

    Delete useless component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,7 +5,6 @@ import CompanyList from '../components/company/CompanyList';
 import { useRecoilValue } from 'recoil';
 import { isLoadingAtoms } from '../../recoil/atoms';
 import LoadingModal from '../components/modal/LoadingModal';
-import Footer from '../components/Footer';
 
 function Home() {
   const isLoading = useRecoilValue(isLoadingAtoms);
@@ -21,7 +20,6 @@ function Home() {
       <Header />
       <SearchBar />
       <CompanyList />
-      <Footer />
       {isLoading && <LoadingModal />}
     </div>
   );
