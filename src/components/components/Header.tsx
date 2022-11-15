3583483926135587c3f2bedb04c11e765de64b79
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-11-15 17:44:22 +0900

    Add svg and apply to header

--- src/components/components/Header.tsx
+++ src/components/components/Header.tsx
@@ -1,15 +1,23 @@
 import { css } from '@emotion/react';
+// @ts-ignore
+import { ReactComponent as QuestionMark } from '../../assets/icon/QuestionMark.svg';
 
 function Header() {
   return (
     <div css={css({ background: '#777D71', minHeight: '4rem' })}>
       <div
         css={css({
+          display: 'flex',
+          justifyContent: 'space-between',
+          width: 'calc(100% - 2rem)',
           padding: '1rem',
         })}
       >
         <span css={css({ '&:hover': { cursor: 'pointer' } })} onClick={() => window.location.reload()}>
           <img css={css({ height: '2rem' })} src={require('../../assets/icon/title.png')} />
+        </span>
+        <span css={css({ '&:hover': { cursor: 'pointer' } })} onClick={() => window.location.reload()}>
+          <QuestionMark width={'2rem'} height={'2rem'} />
         </span>
       </div>
     </div>
