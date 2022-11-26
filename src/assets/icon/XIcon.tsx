cea207f6f01cb297447bca98d4be90b8be142142
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-11-26 21:19:10 +0900

    Create XIcon.tsx

---
+++ src/assets/icon/XIcon.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+type Props = {
+  width: string;
+  height: string;
+};
+
+const SvgComponent = (props: Props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M18 6 6 18M6 6l12 12" />
+  </svg>
+);
+
+export default SvgComponent;
