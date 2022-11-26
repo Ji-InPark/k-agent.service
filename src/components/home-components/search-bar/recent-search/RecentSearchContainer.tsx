63f014303b1ac0f1fafd6b668ca187e04a720ad5
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-11-26 11:37:37 +0900

    Set RecentSearchContainer layout

--- src/components/home-components/search-bar/recent-search/RecentSearchContainer.tsx
+++ src/components/home-components/search-bar/recent-search/RecentSearchContainer.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { css } from '@emotion/react';
 
 function RecentSearchContainer() {
-  return <div></div>;
+  return (
+    <div
+      css={css({
+        display: 'flex',
+        justifyContent: 'center',
+        height: '2rem',
+      })}
+    ></div>
+  );
 }
 
 export default RecentSearchContainer;
