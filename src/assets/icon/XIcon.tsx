b96ed51e3f4aa21c0e44edfcff4d6ca8f8ce5c88
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-11-27 01:16:56 +0900

    Modify svg size

--- src/assets/icon/XIcon.tsx
+++ src/assets/icon/XIcon.tsx
@@ -6,7 +6,16 @@ type Props = {
 };
 
 const SvgComponent = (props: Props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    viewBox={'0 0 20 20'}
+    {...props}
+  >
     <path d="M18 6 6 18M6 6l12 12" />
   </svg>
 );
