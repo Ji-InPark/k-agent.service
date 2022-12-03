7e3a58e24f11d0224b651dbcebaf68505f87b342
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-12-03 17:31:38 +0900

    Update index.ts

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const search = axios;
 
-search.defaults.baseURL = process.env.REACT_APP_ENVIRONMENT == 'development' ? 'localhost:8080/api' : 'api.k-agent.services/api';
+search.defaults.baseURL = process.env.REACT_APP_ENVIRONMENT == 'development' ? 'localhost:8080/api' : 'http://api.k-agent.services/api';
 
 export default search;
