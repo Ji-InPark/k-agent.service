cd423149c9516f616c46936b41dbe41bae436c58
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-05-07 02:10:09 +0900

    Refactor RecentSearchContainer

--- src/components/home-components/search-bar/recent-search/RecentSearchContainer.tsx
+++ src/components/home-components/search-bar/recent-search/RecentSearchContainer.tsx
@@ -7,19 +7,6 @@ import RecentSearchWord from './RecentSearchWord';
 function RecentSearchContainer() {
   const recentSearchWords = useRecoilValue(recentSearchWordsAtoms);
 
-  if (!recentSearchWords.length)
-    return (
-      <div
-        css={css({
-          display: 'flex',
-          justifyContent: 'center',
-          height: '2rem',
-        })}
-      >
-        최근 검색 기록이 없습니다.
-      </div>
-    );
-
   return (
     <div
       css={css({
@@ -28,9 +15,7 @@ function RecentSearchContainer() {
         height: '2rem',
       })}
     >
-      {recentSearchWords.map((it) => (
-        <RecentSearchWord word={it} />
-      ))}
+      {!recentSearchWords.length ? '최근 검색 기록이 없습니다.' : recentSearchWords.map((it) => <RecentSearchWord word={it} />)}
     </div>
   );
 }
