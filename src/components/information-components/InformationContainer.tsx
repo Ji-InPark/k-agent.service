d4661115a7a4abc991e0f76b3552b8d81de2ad62
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-05-29 17:32:11 +0900

    Update InformationContainer.tsx

--- src/components/information-components/InformationContainer.tsx
+++ src/components/information-components/InformationContainer.tsx
@@ -44,7 +44,8 @@ function InformationContainer() {
 
       <h1>정보 업데이트 이력</h1>
       <ul>
-        <UpdateInformationItem>2022년 12월 30일 (현재 적용)</UpdateInformationItem>
+        <UpdateInformationItem>2023년 05월 29일 (현재 적용)</UpdateInformationItem>
+        <UpdateInformationItem>2022년 12월 30일</UpdateInformationItem>
         <UpdateInformationItem>2022년 10월 26일</UpdateInformationItem>
       </ul>
 
