88291e8df0194b69cebb704a362b4ea1c76f1f5a
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-07-09 01:09:12 +0900

    Implement admin page

--- src/components/pages/Admin.tsx
+++ src/components/pages/Admin.tsx
@@ -1,9 +1,22 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { css } from '@emotion/react';
+import search from '../../axios';
+import { useRecoilState } from 'recoil';
+import { isLoadingAtoms } from '../../recoil/atoms';
+import LoadingModal from '../common/modal/LoadingModal';
 
 function Admin() {
+  const [strings, setStrings] = useState<Array<string>>([]);
+  const [selectedString, setSelectedString] = useState('');
   const [password, setPassword] = useState('');
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useRecoilState(isLoadingAtoms);
+
+  useEffect(() => {
+    search.get<Array<string>>('/serviceTypes').then((response) => {
+      setStrings(response.data);
+    });
+  }, []);
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -24,21 +37,76 @@ function Admin() {
         height: '100vh',
         backgroundColor: 'olive',
         padding: '20px',
+        gap: '20px',
       })}
     >
       <h2 css={css({ marginBottom: '20px', color: 'white' })}>병역일터 정보 업데이트 페이지</h2>
-      <div css={css({ marginBottom: '20px' })}>
+      <div>
         <input type="file" id="file" onChange={handleFileChange} />
       </div>
-      <div css={css({ marginBottom: '20px' })}>
+      <select
+        onChange={(e) => {
+          setSelectedString(e.target.value);
+        }}
+      >
+        <option
+          css={css({
+            textAlign: 'center',
+          })}
+          value=""
+        >
+          선택
+        </option>
+        {strings.map((str) => {
+          return (
+            <option css={css({ textAlign: 'center' })} key={str} value={str}>
+              {str}
+            </option>
+          );
+        })}
+      </select>
+      <div>
         <label htmlFor="password" css={css({ color: 'white' })}>
           비밀번호:
         </label>
         <input type={'password'} id="password" value={password} onChange={handlePasswordChange} />
       </div>
       <div>
-        <button disabled={!selectedFile || !password}>Upload File</button>
+        <button
+          disabled={!selectedFile || !password || !selectedString}
+          onClick={() => {
+            setIsLoading(true);
+            const formData = new FormData();
+            formData.append(
+              'items',
+              new Blob(
+                [
+                  JSON.stringify({
+                    name: 'Book',
+                    quantity: '12',
+                  }),
+                ],
+                {
+                  type: 'application/json',
+                },
+              ),
+            );
+            formData.append('file', selectedFile as File);
+            formData.append('password', password);
+            formData.append('serviceType', selectedString);
+            search
+              .post('company', formData)
+              .then((response) => {
+                const count = response.data.companyCount;
+                alert(count ? count : response.data);
+              })
+              .finally(() => setIsLoading(false));
+          }}
+        >
+          Upload File
+        </button>
       </div>
+      {isLoading && <LoadingModal />}
     </div>
   );
 }
