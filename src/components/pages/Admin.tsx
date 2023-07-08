74a18b88850593d0af72df71dc4d96c2f29800b1
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-07-09 00:33:45 +0900

    Implement basic admin page ui

--- src/components/pages/Admin.tsx
+++ src/components/pages/Admin.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { css } from '@emotion/react';
 
 function Admin() {
-  return <div></div>;
+  const [password, setPassword] = useState('');
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setSelectedFile(file || null);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div
+      css={css({
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: 'olive',
+        padding: '20px',
+      })}
+    >
+      <h2 css={css({ marginBottom: '20px', color: 'white' })}>병역일터 정보 업데이트 페이지</h2>
+      <div css={css({ marginBottom: '20px' })}>
+        <input type="file" id="file" onChange={handleFileChange} />
+      </div>
+      <div css={css({ marginBottom: '20px' })}>
+        <label htmlFor="password" css={css({ color: 'white' })}>
+          비밀번호:
+        </label>
+        <input type={'password'} id="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <div>
+        <button disabled={!selectedFile || !password}>Upload File</button>
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
