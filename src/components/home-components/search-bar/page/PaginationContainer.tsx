203704fee998ec63f217612155f6e0aa601dea6e
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-07-13 01:01:01 +0900

    Fix pagination error

--- src/components/home-components/search-bar/page/PaginationContainer.tsx
+++ src/components/home-components/search-bar/page/PaginationContainer.tsx
@@ -7,10 +7,11 @@ import useCompany from '../../../../hooks/useCompany';
 function PaginationContainer() {
   const companyResponse = useRecoilValue(companyResponseAtoms);
   const selectedPageNumber = companyResponse.pageable.pageNumber;
-  const firstPageNumberInCurrentContainer = Math.floor(selectedPageNumber / 10) * 10 + 1;
+  const firstPageNumberInCurrentContainer = Math.floor(selectedPageNumber / 10) * 10;
 
-  const currentPageNumberLength = Math.min(Math.max(companyResponse.totalPages - (firstPageNumberInCurrentContainer + 9), 0), 10);
-  const currentPageNumbers = Array.from(Array(currentPageNumberLength)).map((_, index) => firstPageNumberInCurrentContainer + index);
+  const currentPageNumberLength = Math.min(Math.max(companyResponse.totalPages - firstPageNumberInCurrentContainer, 0), 10);
+  const currentPageNumbers = Array.from(Array(currentPageNumberLength)).map((_, index) => firstPageNumberInCurrentContainer + index + 1);
+  console.log(firstPageNumberInCurrentContainer, currentPageNumberLength, currentPageNumbers);
 
   const { changePage } = useCompany();
 
@@ -29,14 +30,13 @@ function PaginationContainer() {
       })}
     >
       <PageButton text="<<" onClick={handleSelectPage(0)} />
-      <PageButton text="<" onClick={handleSelectPage(0)} />
-      {currentPageNumbers.map((it) => {
-        const pageNumber = firstPageNumberInCurrentContainer + Number(it);
+      <PageButton text="<" onClick={handleSelectPage(Math.max(firstPageNumberInCurrentContainer - 10, 0))} />
+      {currentPageNumbers.map((pageNumber) => {
         if (pageNumber > companyResponse.totalPages) return null;
-        return <PageButton key={it} text={it} onClick={handleSelectPage(Number(it) - 1)} />;
+        return <PageButton key={pageNumber} text={pageNumber} onClick={handleSelectPage(Number(pageNumber) - 1)} />;
       })}
       <PageButton text=">" onClick={handleSelectPage(firstPageNumberInCurrentContainer + 10)} />
-      <PageButton text=">>" onClick={handleSelectPage(companyResponse.totalPages - 1)} />
+      <PageButton text=">>" onClick={handleSelectPage(Math.floor(companyResponse.totalPages / 10) * 10)} />
     </div>
   );
 }
