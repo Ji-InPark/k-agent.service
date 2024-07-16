38980b48cdc3b588419b2a5379dbe54d87f48245
Kangjae Choi <awpe89@gmail.com>
2024-07-16 16:45:01 +0900

    refactor: rollback baseURL, improve create axios instance code

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const search = axios.create({
-  baseURL: 'https://kagent.fly.dev/api',
+  baseURL: process.env.REACT_APP_ENVIRONMENT === 'development' ? 'http://localhost:8080/api' : 'https://kagent.fly.dev/api',
 });
 
 export default search;
